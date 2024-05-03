b9d956601d8ab78d90ca213143d3712d7586fbea
MohammadMahdiHeshmati <mmh.ksx.2002@gmail.com>
2024-05-03 14:49:41 +0330

    fix theme bug

--- app/pages/Loading.js
+++ app/pages/Loading.js
@@ -1,9 +1,14 @@
 import { Animated, StyleSheet, Text, View } from "react-native";
 import React, { useEffect } from "react";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import { useColorScheme } from "nativewind";
 
 const Loading = () => {
   const rotationValue = new Animated.Value(0);
+  const { colorScheme } = useColorScheme();
+  const colors = {
+    bg: colorScheme === "dark" ? "bg-slate-900" : "bg-white",
+  };
 
   const interpolatedRotation = rotationValue.interpolate({
     inputRange: [0, 360],
@@ -27,7 +32,7 @@ const Loading = () => {
   }, []);
 
   return (
-    <View className="w-full h-full flex justify-center items-center">
+    <View className={`w-full h-full flex justify-center items-center ${colors.bg}`}>
       <Animated.View style={styles.myView}>
         <Ionicons name="reload-circle-outline" color={"red"} size={80} />
       </Animated.View>
